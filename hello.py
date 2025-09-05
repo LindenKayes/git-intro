f1c513fb7f5cab37f765833599faeb8a1b573a13
Grace O'Leary <hcps-olearygb@henricostudents.org>
2025-09-05 11:14:16 -0400

    EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEVILLLL

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 print("hello world!")
 print("NO thank you")
 print("man it would be cooler if this was in java wouldn't it?? hmmm? or javascript?")
+print("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeevil")
 
 for i in range(10):
 	print("a")
