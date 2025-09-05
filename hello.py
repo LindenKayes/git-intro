949e77bb4ea42e549ef051e3953617810cbebd5b
Kartikeya jain <gamermind2008@gmail.com>
2025-09-05 11:09:15 -0400

    initial commit after fork

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 print("hello world!")
 print("NO thank you")
 print("man it would be cooler if this was in java wouldn't it?? hmmm? or javascript?")
+print("haha i forked you")
 
 for i in range(10):
 	print("a")
